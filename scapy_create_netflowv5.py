841f322c0f90993cafc2619b72ca0fbfca6b7a74
rgrapython <437957708@qq.com>
2019-05-14 19:10:39 +0800

    Update scapy_create_netflowv5.py

--- scapy_create_netflowv5.py
+++ scapy_create_netflowv5.py
@@ -7,8 +7,6 @@ def create_pks_512ips():
         keyvalue['https flood'] = (6, 24, 443)
         #keyvalue['http flood'] = (6, 24, 80)
         aaa = PcapWriter('d:\\0515.pcap', append=True, sync=True)
-        j = 1
-        k = 1
         i = 1
         #Query ip:http://ip.yqie.com/cn/guangdong/
         gdprovice_yd_ips = [('43.254.159.' + str(i)) for i in range(1,201)]
@@ -16,28 +14,24 @@ def create_pks_512ips():
         uk_ips = [('2.20.183.' + str(i)) for i in range(1, 113)]
         ips_all = gdprovice_yd_ips + gdprovice_nyd_ips + uk_ips
         for ip in ips_all:
-            for key in keyvalue.keys():
-                src_ip = '27.45.167.100'
-                dst_ip =  ip #ip from foshan of gd
-                nexthop = '90.7.1.1'
-                #src_ip = '123::1'
-                #dst_ip = '134::1'
-                #nexthop = '233::1'
-                a = keyvalue[key][0]
-                b = keyvalue[key][1]
-                c = keyvalue[key][2]
-                input = random.randint(1, 65535)
-                output = random.randint(1, 65535)
-                dpkts = 100
-                dOctets = 10000
+            src_ip = '27.45.167.100'
+            dst_ip =  ip #ip from foshan of gd
+            nexthop = '90.7.1.1'
+            a = keyvalue[key][0]
+            b = keyvalue[key][1]
+            c = keyvalue[key][2]
+            input = random.randint(1, 65535)
+            output = random.randint(1, 65535)
+            dpkts = 100
+            dOctets = 10000
 
-                pks = Ether(src='00:0C:29:31:3A:72', dst='00:0C:29:7A:00:D9') / IP(dst='10.242.39.6', src='10.10.10.10') / UDP(dport=9999, sport=3333) / NetflowHeader(
-                    version=5) / NetflowHeaderV5(count=1, sysUptime=7.00, unixSecs=1539751877, unixNanoSeconds=261158,
-                                                 flowSequence=9789, engineType=0, engineID=0) / NetflowRecordV5(
-                    src=src_ip, dst=dst_ip, nexthop=nexthop, input=input, output=output, dpkts=dpkts, dOctets=dOctets,
-                    tcpFlags=b, prot=a, srcport=c)
-                aaa.write(pks)
-                break
+            pks = Ether(src='00:0C:29:31:3A:72', dst='00:0C:29:7A:00:D9') / IP(dst='10.242.39.6', src='10.10.10.10') / UDP(dport=9999, sport=3333) / NetflowHeader(
+                version=5) / NetflowHeaderV5(count=1, sysUptime=7.00, unixSecs=1539751877, unixNanoSeconds=261158,
+                                             flowSequence=9789, engineType=0, engineID=0) / NetflowRecordV5(
+                src=src_ip, dst=dst_ip, nexthop=nexthop, input=input, output=output, dpkts=dpkts, dOctets=dOctets,
+                tcpFlags=b, prot=a, srcport=c)
+            aaa.write(pks)
+                
             i += 1
             if i > 2000:
                 break
