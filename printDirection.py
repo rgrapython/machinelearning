c19e6c60f316198b4910914fc94cee33f4f2194a
rgrapython <437957708@qq.com>
2019-07-09 15:51:20 +0800

    Update printDirection.py

--- printDirection.py
+++ printDirection.py
@@ -4,6 +4,25 @@
 #########################################################################
 import dpkt
 import socket
+import pygeoip
+import optparse
+
+gi = pygeoip.GeoIP("/opt/GeoIP/Geo.dat")
+
+def retGeoStr(ip):
+    try:
+         rec = gi.record_by_name(ip)
+         city = rec['city']
+         country = rec['country_code3']
+         if (city != ''):
+            geoLoc = city + ", " + country
+          else:
+            geoLoc = country
+          return geoLoc
+     except Exception,e:
+        print e
+        return "Unregistered"
+    
 def printPcap(pcap):
     for (ts, buf) in pcap:
         try:
@@ -12,9 +31,13 @@ def printPcap(pcap):
             src = socket.inet_ntoa(ip.src)
             dst = socket.inet_ntoa(ip.dst)
             print '[+] Src: ' + src + '--> Dst: ' + dst
+            print '[+] Src: ' + retGeoStr(src) + ' --> Dst: '+ retGeoStr(dst)
          except:
             pass
 def main():
+    parser = optparse.OptionParser('usage%prog -p <pcap file>')
+    parser.add_option('-p', dest='pcapFile', type='string', help='specify pcap filename')
+    (options, args)
     f = open('geotest.pcap')  #创建一个文件对象
     pcap = dpkt.pcap.Reader(f) #创建一个pcap.reader类的对象
     printPcap(pcap)
